056f8795a1a2045eda0bef02d8eedebc5e02414e
Pveen3113 <praveen@student.usm.my>
2021-10-27 01:02:26 +0800

    Build the main loop of the game and completed the ball movemeents and score points

--- game.py
+++ game.py
@@ -95,3 +95,48 @@ wn.onkeypress(paddle_a_left, "a")
 
 wn.onkeypress(paddle_b_up, "Up")  # when the user presses w , call the function paddle a up
 wn.onkeypress(paddle_b_down, "Down")
+
+# Main game loop
+while True:
+    wn.update()  # everytime the game runs it updates the screen
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1  # Reverse the direction of the ball
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {} ".format(score_a, score_b), align="center",
+                  font=("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {} ".format(score_a, score_b), align="center",
+                  font=("Courier", 24, "normal"))
+
+    # Paddle and ball collision
+    # in between up and down of the paddle
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (
+            ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (
+            ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
